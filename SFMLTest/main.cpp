4c001bd1827e78ec642a072dbd0442f15d06e04b
nambona890 <zander.morrow@gmail.com>
2020-01-15 02:00:39 +1100

    changed the reset key to a single-press event

--- SFMLTest/main.cpp
+++ SFMLTest/main.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	srand((unsigned int)time(NULL));
 	sf::RenderWindow window(sf::VideoMode(SCREENWIDTH, SCREENHEIGHT), "ur mom");
+	window.setKeyRepeatEnabled(false);
 	sf::Texture muncherTex[2];
 	muncherTex[0].loadFromFile("munch1.png");
 	muncherTex[1].loadFromFile("munch2.png");
@@ -26,12 +27,14 @@ int main()
 		}
 		sf::Event event;
 		while (window.pollEvent(event))
-			if (event.type == sf::Event::Closed) window.close();
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::R))
 		{
-			munchers.clear();
-			for (int i = 0; i < 8; i++)
-				munchers.push_back(Muncher(&muncherTex[munindex]));
+			if (event.type == sf::Event::Closed) window.close();
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Key::R)
+			{
+				munchers.clear();
+				for (int i = 0; i < 8; i++)
+					munchers.push_back(Muncher(&muncherTex[munindex]));
+			}
 		}
 		window.clear(sf::Color{0,128,128});
 		for (int i = 0; i < 8; i++)
